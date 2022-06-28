50ba8c5da33e705639e0776131ae9b7cfea5ba86
Jordan <jyew1992@gmail.com>
2022-06-28 16:16:10 +0800

    Update task.py

--- task.py
+++ task.py
@@ -5,9 +5,9 @@ from datetime import datetime
 def get_bitcoin_price():
     api_link = 'https://api.coindesk.com/v1/bpi/currentprice.json'
     response = requests.get(api_link)
-    print(response.json())
+    print(response.json(), flush=True)
 
 if __name__ == "__main__":
-    print(f"Running script at {datetime.now()}")
+    print(f"Running script at {datetime.now()}", flush=True)
     get_bitcoin_price()
     sys.stdout.flush()
