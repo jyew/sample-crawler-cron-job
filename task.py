79086e2d12e4e034e1bd76887e6b6628aac5ae3c
Jordan <jyew1992@gmail.com>
2022-06-29 10:29:53 +0800

    Update task.py

--- task.py
+++ task.py
@@ -1,6 +1,8 @@
+from pprint import pprint
 import requests
 import sys
 import logging
+import pprint
 from datetime import datetime
 
 log = logging.getLogger("my-logger")
@@ -8,7 +10,7 @@ log = logging.getLogger("my-logger")
 def get_bitcoin_price():
     api_link = 'https://api.coindesk.com/v1/bpi/currentprice.json'
     response = requests.get(api_link)
-    print(response.json(), flush=True)
+    pprint.pprint(response.json())
 
 if __name__ == "__main__":
     print(f"Running script at {datetime.now()}", flush=True)
